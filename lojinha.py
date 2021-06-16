04ad6aa076500a14531fc0f7645dcaa8b0e1803b
Davi Mordonho <85120918+Davi-Lv@users.noreply.github.com>
2021-06-15 21:32:40 -0300

    Update lojinha.py

--- lojinha.py
+++ lojinha.py
@@ -1,12 +1,7 @@
 
-print('===================')
-print('      lojinha      ')
-print('===================')
-
-
 total = 0
 while True:
-    codigo = int(input('escolha os codigos dos produtos\n1. R$5,00\n2. R$8,00\n3. R$10,00\n5. R$13,00\n9. R$16,00\n0. total.\n'))
+    codigo = int(input('escolha os codigos dos produtos\n1. R$5,00\n2. R$8,00\n3. R$10,00\n4. R$13,00\n5. R$16,00\n0. total.\n'))
     if codigo == 0:
         print(f'total = R${total:.2f}')
         break
@@ -18,9 +13,9 @@ while True:
             total = total + quantidade * 8
         elif codigo == 3:
             total = total + quantidade * 10
-        elif codigo == 5:
+        elif codigo == 4:
             total = total + quantidade * 13
-        elif codigo == 9:
+        elif codigo == 5:
             total = total + quantidade * 6
     else:
         print('codigo invalido')
