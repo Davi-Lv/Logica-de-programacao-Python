f6992a3b07051d598d770e35716abae0f898ccb7
Davi Mordonho <85120918+Davi-Lv@users.noreply.github.com>
2021-06-20 18:01:58 -0300

    soma

---
+++ soma de 2 numeros inteiros.py
@@ -0,0 +1,29 @@
+print('______________________________________')
+print('|              -Quest 6-             |')
+print('|     -soma dos numeros inteirios    |')
+print('|digite "0" para finalizar o programa|')
+print('|____________________________________|')
+print('')
+
+
+
+
+
+while True:
+    x = int(input('valor 1: '))
+    if x == 0:
+        print('PROGRAMA ENCERRADO!!')
+        break
+    y = int(input('valor 2: '))
+    if y == 0:
+        print('PROGRAMA ENCERRADO!!')
+        break
+    if x < y:
+        print('o primeiro valor deve ser maior que o segundo')
+    elif x < 0 or y < 0:
+        print('os 2 valores devem ser positivos')
+    else:
+        soma = x+y
+        print(f'soma dos valores = {soma}')
+    print('----------------------')
+    
