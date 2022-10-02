73187a5d0cad6eb853df351072864109f567add2
Davi Mordonho <85120918+Davi-Lv@users.noreply.github.com>
2022-10-02 17:06:22 -0300

    Add files via upload

---
+++ index, top 3 e os 2 ultimos.py
@@ -0,0 +1,12 @@
+top = ('Estados Unidos', 'China', 'Japão', 'Grã-Bretanha', 'Atletas da Rússia','Austrália', 'Holanda',
+ 'França', 'Alemanha', 'Itália', 'Canadá', 'Brasil', 'Nova Zelandia', 'Cuba', 'Hungria')
+
+i = top[:3]
+
+ii = top[-2:]
+
+iii = top.index('Brasil')
+
+print(f'top 3: {i}')
+print(f'ultimos 2 colocados: {ii}')
+print(f'brasil na posição: {iii}')
